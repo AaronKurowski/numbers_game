e1d00958b527d2b624722d3aa6afb0fa60b2a1b1
Aaron Kurowski <aaron12342@att.net>
2024-08-18 21:11:05 -0500

    on the dom

--- game.js
+++ game.js
@@ -1,48 +1,60 @@
 let numbers = {1: null, 2: null, 3: null};
 let userRolling = true;
-let userPlacing = true;
 let currentRoll = 0;
 let gameSize = 3;
 let gameBoundLeft = 1;
 let gameBoundRight = 10;
 let didWin = false;
+let numberList = document.querySelector("[data-game]");
+let numberDisplay = document.querySelector("[data-current-roll");
+let msg = document.querySelector("[data-msg]");
 
-while (userRolling) {
-    // generate a random number 
-    currentRoll = Math.floor(Math.random() * (gameBoundRight - gameBoundLeft + 1) + gameBoundLeft)
 
-    if (Object.values(numbers).some(el => el === currentRoll)) continue;
+// generate a random number 
+currentRoll = Math.floor(Math.random() * (gameBoundRight - gameBoundLeft + 1) + gameBoundLeft);
 
-    // display number
-    console.log(currentRoll);
+// display number
+numberDisplay.innerHTML = "Current Roll: " + currentRoll;
 
-    // ask user where they want to place the number
-    // it must be between 1-20
-    userPlacing = true;
-    while (userPlacing) {
-        let userPlacement = prompt("Place 1-" + gameSize);
-        let placeIsntTaken = numbers[userPlacement] === null;
+// take user input for placing
+document.querySelector("[data-user-place-btn]").addEventListener("click", e => {
+    let thisButton = e.target;
+    let userPlacement = e.target.previousElementSibling.value;
+    let placeIsntTaken = numbers[userPlacement] === null;
 
-        if (userPlacement >= 1 && userPlacement <= gameSize && placeIsntTaken) {
-            numbers[userPlacement] = currentRoll;
-            userPlacing = false;
-        } else {
-            console.log("Invalid placement");
-        }
+    if (userPlacement >= 1 && userPlacement <= gameSize && placeIsntTaken) {
+        numbers[userPlacement] = currentRoll;
+        numberList.children[userPlacement - 1].innerHTML = currentRoll;
+    } else {
+        msg.innerHTML = "Invalid placement <br> Placements should be between <b>1</b> and <b>" + gameSize + "</b>";
+        return;
     }
 
-    console.table(numbers);
+    // check if game all have been filled
     userRolling = Object.values(numbers).some(el => el === null) ? true : false;
-}
+    
+    if (! userRolling) {
+        // check if won
+        didWin = Object.values(numbers).every((x, i) => {
+            return i === 0 || x >= Object.values(numbers)[i - 1];
+        });
 
-// check if won
-didWin = Object.values(numbers).every((x, i) => {
-    return i === 0 || x >= Object.values(numbers)[i - 1];
+        thisButton.disabled = true;
+
+        if (didWin) {
+            msg.innerHTML = "******YOU WON!!!*******";
+        } else {
+            msg.innerHTML = "YOU LOST";
+        }
+
+        userPlacement.disabled = true;
+    } else {
+        // generate a random number 
+        while (Object.values(numbers).some(el => el === currentRoll)) {
+            currentRoll = Math.floor(Math.random() * (gameBoundRight - gameBoundLeft + 1) + gameBoundLeft);
+        }
+
+        // display number
+        numberDisplay.innerHTML = "Current Roll: " + currentRoll;
+    }
 });
-
-console.log("Here are you results: ");
-console.table(numbers);
-
-if (didWin) {
-    console.log("******YOU WON!!!*******");
-}
