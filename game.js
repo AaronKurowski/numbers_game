fdb2bd24d86a539b52b33488329a8b00307cff70
Aaron Kurowski <aaron12342@att.net>
2024-08-22 20:34:54 -0500

    game setup and generation based on params

--- game.js
+++ game.js
@@ -1,23 +1,48 @@
-let numbers = {1: null, 2: null, 3: null};
+let numbers = {};
 let userRolling = true;
 let currentRoll = 0;
 let gameSize = 3;
 let gameBoundLeft = 1;
 let gameBoundRight = 10;
 let didWin = false;
-let numberList = document.querySelector("[data-game]");
+let numberList = document.querySelector("[data-number-list]");
 let numberDisplay = document.querySelector("[data-current-roll");
 let msg = document.querySelector("[data-msg]");
-console.log(msg);
 
-// generate a random number 
-currentRoll = Math.floor(Math.random() * (gameBoundRight - gameBoundLeft + 1) + gameBoundLeft);
+// Two main features
+// 1. Game setup
+// 2. Game play
 
-// display number
-numberDisplay.innerHTML = "Current Roll: " + currentRoll;
+document.querySelector("[data-setup-form]").addEventListener("submit", e => {
+    e.preventDefault();
+    gameSize = parseInt(e.target[0].value);
+    gameBoundLeft = parseInt(e.target[1].value);
+    gameBoundRight = parseInt(e.target[2].value);
+    
+    document.querySelector("[data-game-section]").style.display = "block";
+    
+    e.target.parentElement.remove();
+
+    // generate the list ellies
+    for (let i = 0; i < gameSize; i++) {
+        let element = document.createElement("li");
+        element.id = "placement_" + (i + 1);
+        numberList.appendChild(element);
+
+        Object.assign(numbers, { [i + 1]: null });
+    }
+
+    // generate a random number 
+    currentRoll = Math.floor(Math.random() * (gameBoundRight - gameBoundLeft + 1) + gameBoundLeft);
+
+    // display number
+    numberDisplay.innerHTML = "Current Roll: " + currentRoll;
+});
+
 
 // take user input for placing
 document.querySelector("[data-user-place-btn]").addEventListener("click", e => {
+    msg.innerHTML = "";
     let thisButton = e.target;
     let userPlacement = e.target.previousElementSibling.value;
     let placeIsTaken = numbers[userPlacement] !== null;
@@ -26,7 +51,11 @@ document.querySelector("[data-user-place-btn]").addEventListener("click", e => {
         if (placeIsTaken) {
             msg.innerHTML = "Number already placed there";
         } else {
+
+            // saving in an object idk
             numbers[userPlacement] = currentRoll;
+
+            // list placement
             numberList.children[userPlacement - 1].innerHTML = currentRoll;
         }
     } else {
